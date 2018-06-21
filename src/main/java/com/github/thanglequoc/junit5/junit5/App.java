6d57f80c4fbb0dc947ac0f22ba5dd202228d2cdf
Thang Le Quoc <silverhair.guy@gmail.com>
2018-06-21 14:18:57 +0700

    Init Commit

---
+++ src/main/java/com/github/thanglequoc/junit5/junit5/App.java
@@ -0,0 +1,13 @@
+package com.github.thanglequoc.junit5.junit5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
